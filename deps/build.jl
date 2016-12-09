40d410694ec74728cef18fcd93c874d768510de1
David Little <david.frank.little@gmail.com>
2016-12-09 10:16:12 -0800

    Preparing new binary install method.

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,9 @@
 # modified from: https://github.com/rennis250/SDL.jl/blob/ef125ec/deps/build.jl
 using BinDeps
 
+# TODO: remove BinDeps, and just do a straigtforward download and install
+# from the SDL website.
+
 @BinDeps.setup
 
 libSDL2 = library_dependency("libSDL2", aliases = ["libSDL2", "SDL"])
@@ -9,6 +12,8 @@ libSDL2_mixer = library_dependency("libSDL2_mixer", aliases = ["libSDL2_mixer"],
 libSDL2_ttf = library_dependency("libSDL2_ttf", aliases = ["libSDL2_ttf"], depends = [libSDL2])
 
 @static if is_windows()
+  # TODO: create a function to download and install binaries
+  # from sdl website
   if Pkg.installed("WinRPM") === nothing
     error("WinRPM package not installed, pluse run Pkg.add(\"WinRPM\")")
   end
@@ -18,6 +23,8 @@ libSDL2_ttf = library_dependency("libSDL2_ttf", aliases = ["libSDL2_ttf"], depen
 	provides(WinRPM.RPM, "SDL2_mixer", libSDL2_mixer, os = :Windows)
 	provides(WinRPM.RPM, "SDL2_ttf", libSDL2_ttf, os = :Windows)
 elseif is_apple()
+  # TODO: create function to install mac os x DMG fiels
+
 	if Pkg.installed("Homebrew") === nothing
 		error("Hombrew package not installed, please run Pkg.add(\"Homebrew\")")
 	end
@@ -27,6 +34,8 @@ elseif is_apple()
 	provides(Homebrew.HB, "sdl2_mixer", libSDL2_mixer, os = :Darwin)
 	provides(Homebrew.HB, "sdl2_ttf", libSDL2_ttf, os = :Darwin)
 elseif is_linux()
+  # TODO: create a function to download and install binaries
+  # from sdl website
     provides(AptGet,
 	    	     Dict("libsdl2-2.0-0" => libSDL2,
 	    	          "libsdl2-image-2.0-0" => libSDL2_image,
@@ -60,7 +69,7 @@ function addheaders(source,header_location,uri)
   end
 end
 
-addheaders("SDL2-2.0.1","include","https://www.libsdl.org/release/")
+addheaders("SDL2-2.0.5","include","https://www.libsdl.org/release/")
 addheaders("SDL2_mixer-2.0.1",".","https://www.libsdl.org/projects/SDL_mixer/release/")
 
 rm(downloaddir,force=true,recursive=true)
