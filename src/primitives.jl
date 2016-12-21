1d00e3376c31014e774cbcc34b9d9308e236c99d
David Little <david.frank.little@gmail.com>
2016-12-21 13:33:12 -0800

    argument parsing can now read in additional arguments.

--- src/primitives.jl
+++ src/primitives.jl
@@ -1,3 +1,5 @@
+# TODO: create a 2AFC adaptive abstraction
+
 export instruct, response, addbreak_every, show_cross, @read_args
 using ArgParse
 
@@ -41,23 +43,95 @@ function show_cross(delta_t::Number=0;render_options...)
   moment(delta_t,t -> display(c))
 end
 
-macro read_args(description)
-  quote
-    begin
-      s = ArgParseSettings(description = $(esc(description)))
-      @add_arg_table s begin
-        "sid"
-          $(esc(:help)) = "Subject id. Trials are randomized per subject."
-          $(esc(:required)) = true
-          $(esc(:arg_type)) = String
-        "skip"
-          $(esc(:help)) = "# of offsets to skip. Useful for restarting in middle of experiment."
-          $(esc(:required)) = false
-          $(esc(:arg_type)) = Int
-          $(esc(:default)) = 0
+function as_arg(expr)
+  if isa(expr,Symbol) || expr.head != :kw
+    error("Expected keyword parameters specifying additional program arguments.")
+  end
+
+  if isa(expr.args[2],Symbol)
+    quote
+      $(string(expr.args[1]))
+      $(esc(:required)) = true
+      $(esc(:arg_type)) = $(expr.args[2])
+    end
+  elseif expr.args[2].head == :vect
+    quote
+      $(string(expr.args[1]))
+      $(esc(:required)) = true
+      $(esc(:arg_type)) = String
+      $(esc(:help)) = $(join(map(x -> x.args[1],expr.args[2].args),", "," or "))
+    end
+  else
+    error("Expected keyword value to be a vector of symbols or a type.")
+  end
+end
+
+function as_arg_checker(expr)
+  if !isa(expr.args[2],Symbol) && expr.args[2].head == :vect
+    quote
+      let str = $(string(expr.args[1])), vals = $(expr.args[2])
+        if !any(s -> string(s) == parsed[str],vals)
+          println("Expected \"$str\" argument to be "*join(vals,", "," or ")*".")
+          println(usage_string(s))
+          exit()
+        end
       end
-      parsed = parse_args(ARGS,s)
-      parsed["sid"],parsed["skip"]
+    end
+  else
+    :nothing
+  end
+end
+
+function as_arg_result(expr)
+  if !isa(expr.args[2],Symbol) && expr.args[2].head == :vect
+    :(Symbol(parsed[$(string(expr.args[1]))]))
+  else
+    :(parsed[$(string(expr.args[1]))])
+  end
+end
+
+macro read_args(description,keys...)
+  arg_expr = quote
+    "sid"
+    $(esc(:help)) = "Subject id. Trials are randomized per subject."
+    $(esc(:required)) = true
+    $(esc(:arg_type)) = String
+  end
+
+  for arg_body in map(as_arg,keys)
+    for line in arg_body.args
+      push!(arg_expr.args,line)
     end
   end
+
+  skip_expr = quote
+    "skip"
+    $(esc(:help)) = "# of offsets to skip. Useful for restarting in middle of experiment."
+    $(esc(:required)) = false
+    $(esc(:arg_type)) = Int
+    $(esc(:default)) = 0
+  end
+  arg_expr.args = vcat(arg_expr.args,skip_expr.args)
+
+  body = quote
+    s = ArgParseSettings(description = $(esc(description)))
+
+    @add_arg_table s begin
+      $arg_expr
+    end
+
+    parsed = parse_args(ARGS,s)
+  end
+
+  for line in map(as_arg_checker,keys)
+    push!(body.args,line)
+  end
+
+  result_tuple = :((parsed["sid"],parsed["skip"]))
+  for result = map(as_arg_result,keys)
+    push!(result_tuple.args,result)
+  end
+  push!(body.args,result_tuple)
+
+  body
 end
