ac84c64b9fd06eeccedf46e4e7cce49209d41f22
David Little <david.frank.little@gmail.com>
2017-01-12 09:06:37 -0800

    Mac OS X build now installs previously missing dependencies.

--- deps/build.jl
+++ deps/build.jl
@@ -45,20 +45,16 @@ end
     end
   end
 elseif is_apple()
-  # since the Homebrew.jl binaries don't work and the SDL website uses a
-  # framework, which you can't easily link to via julia, I'm just downloading
-  # the binaries I generated in homebrew from my personal website.
-  try
-    tarfile = joinpath(downloaddir,"SDL2.tgz")
-    download("http://haberdashpi.github.io/SDL2-2.0.5_macosx_binaries.tgz",tarfile)
-    run(`tar xzf $tarfile --directory=$bindir`)
+  using Homebrew
 
-    SDL2 = joinpath(bindir,"libSDL2-2.0.0.dylib")
-    SDL2_mixer = joinpath(bindir,"libSDL2_mixer-2.0.0.dylib")
-    SDL2_ttf = joinpath(bindir,"libSDL2_ttf-2.0.0.dylib")
-  finally
-    rm(downloaddir,recursive=true,force=true)
-  end
+  Homebrew.add("sdl2")
+  Homebrew.add("sdl2_mixer")
+  Homebrew.add("sdl2_ttf")
+
+  prefix = joinpath(Homebrew.prefix(),"lib")
+  SDL2 = joinpath(prefix,"libSDL2-2.0.0.dylib")
+  SDL2_mixer = joinpath(prefix,"libSDL2_mixer-2.0.0.dylib")
+  SDL2_ttf = joinpath(prefix,"libSDL2_ttf-2.0.0.dylib")
 
   deps = Pkg.dir("Weber","deps","deps.jl")
   open(deps,"w") do s
@@ -67,9 +63,6 @@ elseif is_apple()
     end
   end
 elseif is_linux()
-  # using BinDeps
-  # @BinDeps.setup
-
   # SDL2 = library_dependency("libSDL", aliases = ["libSDL", "SDL"])
   # SDL2_mixer = library_dependency("libSDL_mixer", aliases = ["libSDL_mixer"], depends = [libSDL], os = :Unix)
   # SDL2_ttf = library_dependency("libSDL_ttf", aliases = ["libSDL_ttf"], depends = [libSDL], os = :Unix)
@@ -84,7 +77,9 @@ elseif is_linux()
   #   		    "SDL_mixer-devel" => SDLmixer,
   #   		    "SDL_ttf-devel" => SDLttf})
 
-  # @BinDeps.install [:SDL2 => :SDL2]
+  # @BinDeps.install [:SDL2 => :SDL2,
+  #                   :SDL2_mixer => :SDL2_mixer,
+  #                   :SDL2_ttf => :SDL2_ttf]
 
   error("I don't have access to an available linux distro to troubleshoot"*
         " this program, so I have left the linux install unimplemented.")
