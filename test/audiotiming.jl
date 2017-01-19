169c3c29f78ade5be71d8ef995a0c05d29637778
ACN lab <acnllb@ad.unlv.edu>
2017-01-19 08:27:40 -0800

    Updated audio timing test to use lower latency sound setup.

--- test/audiotiming.jl
+++ test/audiotiming.jl
@@ -8,7 +8,7 @@ using DataFrames: DataFrame, writetable
 # from the recorded audio, and determine the timing accuracy of audio playback.
 setup_sound(buffer_size=16)
 timing = 0.2abs.(randn(Float64,100)) + 0.11
-beep = attenuate(ramp(tone(1000,0.05)),25)
+beep = sound(attenuate(ramp(tone(1000,0.05)),25))
 
 exp = Experiment(name = "audio_timing",latency = current_sound_latency())
 
