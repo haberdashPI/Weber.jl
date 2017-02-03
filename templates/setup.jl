dc67b96f249a052229681e4a9be09430b849e2f9
David Little <david.frank.little@gmail.com>
2017-02-03 13:46:05 -0800

    Improved formatting of setup.jl template

--- templates/setup.jl
+++ templates/setup.jl
@@ -1,12 +1,14 @@
 Pkg.add("Weber",{{version}})
 open("calibrate.jl","w") do s
-  println(s,"# call run_calibrate() to select an appropriate attenuation.")
-  println(s,"const atten_dB = 30")
-  println(s,"")
-  println(s,"# call Pkg.test(\"Weber\"). If the timing test fails, increase ")
-  println(s,"# moment resolution to avoid warnings.")
-  println(s,"const moment_resolution = Weber.default_moment_resolution")
-  println(s,"")
-  println(s,"# increase buffer size if you are hearing audible glitches in the sound.")
-  println(s,"const buffer_size = 256")
+  print(s,"""
+  # call run_calibrate() to select an appropriate attenuation.
+  const atten_dB = 30
+
+  # call Pkg.test(\"Weber\"). If the timing test fails, increase
+  # moment resolution to avoid warnings.
+  const moment_resolution = Weber.default_moment_resolution
+
+  # increase buffer size if you are hearing audible glitches in the sound.
+  const buffer_size = 256
+  """)
 end
