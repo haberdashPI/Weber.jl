a15dfe6b1385166b7fc25a2405099cd21a0e28be
David Little <david.frank.little@gmail.com>
2017-02-17 11:53:22 -0800

    Added deployment script for documentation.

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,8 @@
 using Documenter, Weber, Colors, Weber.Cedrus
 makedocs(modules = [Weber,Weber.Cedrus])
+deploydocs(
+  deps = Deps.pip("mkdocs","python-markdown-math"),
+  repo = "github.com/haberdashPI/Weber.jl.git",
+  julia = "0.5",
+  osname = "osx"
+)
