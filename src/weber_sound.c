772526f2456392406c56a14a8b430780f2aa0d1d
David Little <david.frank.little@gmail.com>
2017-02-28 12:37:50 -0800

    Improved latency of playback.

--- src/weber_sound.c
+++ src/weber_sound.c
@@ -16,9 +16,9 @@ typedef struct{
 }Sound;
 
 typedef struct{
+  int locked;
   __int16_t* buffer;
   PaTime start;
-  int counter;
   int offset;
   int len;
   unsigned long last_buffer_size;
@@ -36,24 +36,31 @@ typedef struct{
 static int ws_callback(const void* in,void* out,unsigned long len,
                        const PaStreamCallbackTimeInfo* time_info,
                        PaStreamCallbackFlags status_flags,void* user_data){
+  // lockless multi-threading: we copy the state of the sound
+  // checking the length last. If the sound changes in the middle of a copy
+  // len will be 0 (since this is the first field set), so we won't play
+  // any of the sound in this case.
   TimedSound* sound = (TimedSound*)user_data;
   __int16_t* output_buffer = (__int16_t*)out;
+  __int16_t* sound_buffer = sound->buffer;
+  PaTime start = sound->start;
+  int offset = sound->offset;
+  int sound_len = sound->len;
 
   PaTime buffer_start = time_info->outputBufferDacTime;
   int i=0;
-  int offset=sound->offset;
-  int sound_len = sound->len;
   int zero_padding = 0;
   int copylen;
   int should_start = FALSE;
+  int new_offset = 0;
 
   sound->last_buffer_size = len;
 
   // if the sound has yet to start...
   if(offset == 0){
     // if the sound should start in this callback...
-    if(buffer_start + sound->samplelen * len > sound->start){
-      zero_padding = (int)floor((sound->start - buffer_start)*sound->samplerate);
+    if(buffer_start + sound->samplelen * len > start){
+      zero_padding = (int)floor((start - buffer_start)*sound->samplerate);
       should_start = TRUE;
     }
 
@@ -68,12 +75,14 @@ static int ws_callback(const void* in,void* out,unsigned long len,
   // copy samples as needed
   if((offset > 0 || should_start) && offset < sound_len){
     copylen = len-zero_padding;
-    if(copylen > sound_len-offset) copylen = sound_len-offset;
-    for(;i<copylen;i++){
-      output_buffer[(i<<1)] = sound->buffer[i+offset];
-      output_buffer[(i<<1)+1] = sound->buffer[i+sound_len+offset];
+    for(;i<copylen && i < sound_len - offset;i++){
+      output_buffer[(i<<1)] = sound_buffer[i+offset];
+      output_buffer[(i<<1)+1] = sound_buffer[i+sound_len+offset];
     }
-    sound->offset += copylen;
+    sound->offset = copylen + offset;
+    // lockless multithreading: if the sound length has changed to zero
+    // (meaning new buffer is about to be specified), don't update the offset
+    if(sound->len == 0) sound->offset = 0;
   }
 
   // pad the remaing buffer (possibly everything) with zeros
@@ -109,6 +118,7 @@ WsState* ws_setup(int samplerate){
   state->sound = (TimedSound*)malloc(sizeof(TimedSound));
   state->sound->offset = 0;
   state->sound->len = 0;
+  state->sound->locked = FALSE;
   state->sound->samplerate = samplerate;
   state->sound->samplelen = 1.0/samplerate;
 
@@ -148,22 +158,26 @@ void ws_play(double now,double playat,Sound* toplay,WsState* state){
 
   // wait for the last sound to stop playing
   if(state->started && state->sound->offset < state->sound->len){
-    sleep_amount = 1000*(sound->len - sound->offset)*sound->samplelen - 100;
-    if(sleep_amount > 200) Pa_Sleep(sleep_amount);
-    while(state->started && state->sound->offset < state->sound->len){
+    sleep_amount = 1000*(sound->len - sound->offset)*sound->samplelen - 20;
+    if(sleep_amount > 20) Pa_Sleep(sleep_amount);
+    while(state->sound->offset < state->sound->len){
       // waiting...
     }
   }
 
-  if(state->started){
-    if(paNoError != (state->errcode = Pa_StopStream(state->stream))) return;
-  }
+  // lockless multi-threading: setting len to 0 first means if the callback is
+  // in the middle copying sound the last thing it will do is set len to 0
+  // (since this is the last field copied), and so no sound will be played.
+  sound->len = 0;
   sound->buffer = toplay->buffer;
-  sound->len = toplay->len;
   sound->offset = 0;
   sound->start = (pa_now - now) + playat;
-  state->started = TRUE;
-  if(paNoError != (state->errcode = Pa_StartStream(state->stream))) return;
+
+  sound->len = toplay->len;
+  if(!state->started){
+    state->started=TRUE;
+    if(paNoError != (state->errcode = Pa_StartStream(state->stream))) return;
+  }
 }
 
 EXPORT
@@ -178,14 +192,19 @@ void ws_play_from(int offset,Sound* toplay,WsState* state){
     }
   }
 
-  if(state->started){
-    if(paNoError != (state->errcode = Pa_StopStream(state->stream))) return;
-  }
+  // lockless multi-threading: setting len to 0 first means if the callback is
+  // in the middle copying sound the last thing it will do is set len to 0
+  // (since this is the last field copied), and so no sound will be played.
+  sound->len = 0;
   sound->buffer = toplay->buffer;
   sound->len = toplay->len;
-  sound->offset = offset;
-  state->started = TRUE;
-  if(paNoError != (state->errcode = Pa_StartStream(state->stream))) return;
+  sound->offset = offset; // offset may be cleared, so set it last
+
+  sound->len = toplay->len;
+  if(!state->started){
+    state->started = TRUE;
+    if(paNoError != (state->errcode = Pa_StartStream(state->stream))) return;
+  }
 }
 
 EXPORT
@@ -200,6 +219,7 @@ int ws_stop(WsState* state){
 EXPORT
 void ws_resume(WsState* state){
   if(state->started == FALSE){
+    state->started = TRUE;
     if(paNoError != (state->errcode = Pa_StartStream(state->stream))) return;
   }
 }
