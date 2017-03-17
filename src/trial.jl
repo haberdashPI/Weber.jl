haberdashPI/Weber.jl

commit 4ca7801e62c0ee8e922be930d5d4d0d023be4ea7

Improved exception and warning messages that occur during a moment

src/trial.jl
@@ -352,35 +352,35 @@ The function `fn` is passed the arguments specified in `args` and `keys`.
 """
 function moment(delta_t::Number,fn::Function,args...;keys...)
   precompile(fn,map(typeof,args))
-  TimedMoment(delta_t,() -> fn(args...;keys...))
+  TimedMoment(delta_t,() -> fn(args...;keys...),stacktrace()[2:end])
 end
 
 function moment(fn::Function,args...;keys...)
-  moment(0,fn,args...;keys...)
+  TimedMoment(0.0,() -> fn(args...;keys...),stacktrace()[2:end])
 end
 
-moment(delta_t::Number) = TimedMoment(delta_t,()->nothing)
-moment() = TimedMoment(0,()->nothing)
+moment(delta_t::Number) = TimedMoment(delta_t,()->nothing,stacktrace()[2:end])
+moment() = TimedMoment(0,()->nothing,stacktrace()[2:end])
 
 const PlayFunction = typeof(play)
 function moment(delta_t::Number,::PlayFunction,x;channel=0)
-  PlayMoment(delta_t,sound(x),channel)
+  PlayMoment(delta_t,sound(x),channel,stacktrace()[2:end])
 end
 function moment(delta_t::Number,::PlayFunction,fn::Function;channel=0)
-  PlayFunctionMoment(delta_t,fn,channel)
+  PlayFunctionMoment(delta_t,fn,channel,stacktrace()[2:end])
 end
 
 const StreamFunction = typeof(stream)
 function moment(delta_t::Number,::StreamFunction,itr,channel::Int)
-  StreamMoment(delta_t,itr,channel)
+  StreamMoment(delta_t,itr,channel,stacktrace()[2:end])
 end
 
 const DisplayFunction = typeof(display)
 function moment(delta_t::Number,::DisplayFunction,x;keys...)
-  DisplayMoment(delta_t,visual(x;keys...))
+  DisplayMoment(delta_t,visual(x;keys...),stacktrace()[2:end])
 end
 function moment(delta_t::Number,::DisplayFunction,fn::Function;keys...)
-  DisplayFunctionMoment(delta_t,fn,keys)
+  DisplayFunctionMoment(delta_t,fn,keys,stacktrace()[2:end])
 end
 
 """
@@ -402,12 +402,12 @@ end
 
 function offset_start_moment(fn::Function=()->nothing,count_trials=false)
   precompile(fn,(Float64,))
-  OffsetStartMoment(fn,count_trials,false)
+  OffsetStartMoment(fn,count_trials,false,stacktrace()[2:end])
 end
 
 function final_moment(fn::Function)
   precompile(fn,())
-  FinalMoment(fn)
+  FinalMoment(fn,stacktrace()[2:end])
 end
 
 """
@@ -426,7 +426,7 @@ function await_response(fn::Function;atleast=0.0)
     precompile(fn,(t,))
   end
 
-  ResponseMoment(fn,() -> nothing,0,atleast)
+  ResponseMoment(fn,() -> nothing,0,atleast,stacktrace()[2:end])
 end
 
 """
@@ -449,12 +449,12 @@ function timeout(fn::Function,isresponse::Function,timeout;atleast=0.0)
     precompile(isresponse,(t,))
   end
 
-  ResponseMoment(isresponse,fn,timeout,atleast)
+  ResponseMoment(isresponse,fn,timeout,atleast,stacktrace()[2:end])
 end
 
 flag_expanding(m::AbstractMoment) = m
 function flag_expanding(m::OffsetStartMoment)
-  OffsetStartMoment(m.run,m.count_trials,true)
+  OffsetStartMoment(m.run,m.count_trials,true,m.trace)
 end
 function flag_expanding(m::ExpandingMoment)
   if m.update_offset

src/experiment.jl
@@ -29,6 +29,16 @@ function experiment_running()
   !isnull(experiment_context[]) && flags(get(experiment_context[])).processing
 end
 
+# internal functions used to update and retrieve the stack trace
+# where the currently running moment was defined (improving error message
+# readability)
+const current_moment_trace = Array{Vector{StackFrame}}()
+update_trace(m::AbstractMoment) =
+  !isempty(moment_trace(m)) ? current_moment_trace[] = moment_trace(m) : nothing
+update_tracek(m::MomentSequence) =
+  current_moment_trace[] = moment_trace(m.data[1])
+moment_trace() = current_moment_trace[]
+
 """
     Weber.trial()
 
@@ -431,9 +441,12 @@ function run{T <: BaseExperiment}(
       end
     end
   catch e
+    if !isempty(moment_trace())
+      print(STDERR,"\nException during a moment defined")
+      foreach(x -> println(STDERR,x),moment_trace())
+    end
     if await_input
-      show(e)
-      Base.show_backtrace(STDOUT,catch_backtrace())
+      showerror(STDERR,e,catch_backtrace())
     else
       rethrow(e)
     end
@@ -486,6 +499,7 @@ function process(exp::Experiment,queue::MomentQueue,event::ExpEvent)
 
   if !isempty(queue)
     moment = front(queue)
+    update_trace(moment)
     handled = handle(exp,queue,moment,event)
     if handled
       prepare!(queue,time(event))
@@ -511,6 +525,7 @@ function process(exp::Experiment,queue::MomentQueue,t::Float64)
         run_time = offset + precise_time()
       end
       data(exp).last_time = run_time
+      update_trace(moment)
       if handle(exp,queue,moment,run_time)
         d = required_delta_t(moment)
         prepare!(queue,run_time)
@@ -526,7 +541,8 @@ function process(exp::Experiment,queue::MomentQueue,t::Float64)
              latency, reduce the amount of slow code in moments, close programs,
              or run on a faster machine. Or, if this amount of latency is
              acceptable, you should increase `moment_resolution` when you call
-             `Experiment`."))
+             `Experiment`.")*"\n"*
+             reduce(*,"",map(x -> string(x)*"\n",moment_trace())))
           record("high_latency",value=latency)
         end
 

src/types.jl
@@ -364,17 +364,33 @@ required_delta_t(m::AbstractMoment) = delta_t(m)
 isimmediate(m::AbstractMoment) = false
 sequenceable(m::AbstractMoment) = false
 
+"""
+    moment_trace(m)
+
+Returns the stacktrace indicating where this moment was defined.
+
+!!! note
+
+    This method is part of the private interface for moments. It should not be
+    called directly, but implemented as part of an extension.  You can get a
+    stacktrace inside the function you define that constructs your custom moment
+    using `stacktrace()[2:end]`.
+"""
+moment_trace(m::AbstractMoment) = Vector{StackFrame}()
+
 type ResponseMoment <: SimpleMoment
   respond::Function
   timeout::Function
   timeout_delta_t::Float64
   minimum_delta_t::Float64
+  trace::StackTrace
 end
 function delta_t(moment::ResponseMoment)
   (moment.timeout_delta_t > 0.0 ? moment.timeout_delta_t : Inf)
 end
 required_delta_t(m::ResponseMoment) = Inf
 isimmediate(m::ResponseMoment) = false
+moment_trace(m::ResponseMoment) = m.trace
 
 abstract AbstractTimedMoment <: SimpleMoment
 sequenceable(m::AbstractTimedMoment) = true
@@ -383,72 +399,87 @@ isimmediate(m::AbstractTimedMoment) = delta_t(m) == 0.0
 type TimedMoment <: AbstractTimedMoment
   delta_t::Float64
   run::Function
+  trace::StackTrace
 end
 delta_t(moment::TimedMoment) = moment.delta_t
 sequenceable(m::TimedMoment) = true
+moment_trace(m::TimedMoment) = m.trace
 
 type OffsetStartMoment <: AbstractTimedMoment
   run::Function
   count_trials::Bool
   expanding::Bool
+  trace::StackTrace
 end
 delta_t(moment::OffsetStartMoment) = 0.0
 isimmediate(m::OffsetStartMoment) = false
 sequenceable(m::OffsetStartMoment) = false
+moment_trace(m::OffsetStartMoment) = m.trace
 
 type PlayMoment <: AbstractTimedMoment
   delta_t::Float64
   sound::Sound
   channel::Int
+  trace::StackTrace
   prepared::Bool
 end
-PlayMoment(d,f,c) = PlayMoment(d,f,c,false)
+PlayMoment(d,f,c,t) = PlayMoment(d,f,c,t,false)
 delta_t(m::PlayMoment) = m.delta_t
 sequenceable(m::PlayMoment) = true
+moment_trace(m::PlayMoment) = m.trace
 
 type PlayFunctionMoment <: AbstractTimedMoment
   delta_t::Float64
   fn::Function
   channel::Int
+  trace::StackTrace
   prepared::Nullable{Sound}
 end
-PlayFunctionMoment(d,f,c) = PlayFunctionMoment(d,f,c,Nullable())
+PlayFunctionMoment(d,f,c,t) = PlayFunctionMoment(d,f,c,t,Nullable())
 delta_t(m::PlayFunctionMoment) = m.delta_t
 sequenceable(m::PlayFunctionMoment) = true
+moment_trace(m::PlayFunctionMoment) = m.trace
 
 type StreamMoment <: AbstractTimedMoment
   delta_t::Float64
   itr
   channel::Int
+  trace::StackTrace
 end
 delta_t(m::StreamMoment) = delta_t
 required_delta_t(m::StreamMoment) = Inf
 isimmediate(m::StreamMoment) = false
 sequenceable(m::StreamMoment) = false
+moment_trace(m::StreamMoment) = m.trace
 
 type DisplayMoment <: AbstractTimedMoment
   delta_t::Float64
   visual::SDLRendered
+  trace::StackTrace
 end
 delta_t(m::DisplayMoment) = m.delta_t
 sequenceable(m::DisplayMoment) = true
+moment_trace(m::DisplayMoment) = m.trace
 
 type DisplayFunctionMoment <: AbstractTimedMoment
   delta_t::Float64
   fn::Function
   keys::Vector
+  trace::StackTrace
   visual::Nullable{SDLRendered}
 end
-DisplayFunctionMoment(d,f,k) = DisplayFunctionMoment(d,f,k,Nullable())
+DisplayFunctionMoment(d,f,k,t) = DisplayFunctionMoment(d,f,k,t,Nullable())
 delta_t(m::DisplayFunctionMoment) = m.delta_t
 sequenceable(m::DisplayFunctionMoment) = true
+moment_trace(m::DisplayFunctionMoment) = m.trace
 
 type FinalMoment <: SimpleMoment
   run::Function
+  trace::StackTrace
 end
 delta_t(moment::FinalMoment) = 0.0
 isimmediate(m::FinalMoment) = true
-
+moment_trace(m::FinalMoment) = m.trace
 
 type CompoundMoment <: AbstractMoment
   data::Array{AbstractMoment}

commit 0ee93969ce6b0aa458a9052558502b70eba041c7

Fixed bug in moment generation
introduced by 4ca7801e62c0ee8e922be930d5d4d0d023be4ea7

src/trial.jl
@@ -1,7 +1,7 @@
 using Lazy: @_
 using DataStructures
 using MacroTools
-import Base: run
+import Base: run, display
 export addtrial, addbreak, addpractice, moment, await_response, record, timeout,
   when, looping, @addtrials
 
@@ -356,7 +356,7 @@ function moment(delta_t::Number,fn::Function,args...;keys...)
 end
 
 function moment(fn::Function,args...;keys...)
-  TimedMoment(0.0,() -> fn(args...;keys...),stacktrace()[2:end])
+  moment(0.0,fn,args...;keys...)
 end
 
 moment(delta_t::Number) = TimedMoment(delta_t,()->nothing,stacktrace()[2:end])