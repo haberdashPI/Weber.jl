haberdashPI/Weber.jl

commit 41ceae3af134ba806eb026aa72615cf851cc805e

Call prepare! just before the first non-zero pause before a moment
This allows a string of delta_t zero moments to occur between prepare! and the
moment prepared, thus increasing the number of cases where prepare! is called
some short time before the moment it preapres is run (rather than immedaitely
before that moment runs).

This functionality is used to allow sounds to be prepared ahead of time, and
thus precisely timed, in some common uses cases that weren't covered by the
previosu functionality.

Closes #23

src/experiment.jl
@@ -1,3 +1,4 @@
+using Lazy: @>, takewhile
 import Base: run
 export Experiment, setup, run, addcolumn
 
@@ -183,7 +184,7 @@ function Experiment(;skip=0,columns=Symbol[],debug=false,
   last_time = 0.0
   next_moment = 0.0
   pause_mode = Running
-  moments = [MomentQueue(Deque{AbstractMoment}(),0.0)]
+  moments = [MomentQueue()]
   streamers = Dict{Int,Streamer}()
   cleanup = () -> error("no cleanup function available!")
   last_good_delta = -1.0
@@ -232,6 +233,7 @@ function setup{T <: BaseExperiment}(fn::Function,exp::T)
 
     # setup all trial moments for this experiment
     experiment_context[] = Nullable(top(exp))
+    addmoment(top(exp),moment())
     fn()
     experiment_context[] = Nullable{Experiment}()
 
@@ -378,6 +380,7 @@ function run{T <: BaseExperiment}(
 
     start = precise_time()
     tick = data(exp).last_time = last_input = last_delta = 0.0
+    prepare!(data(exp).moments[1],Inf)
     while flags(exp).processing
       tick = data(exp).last_time = precise_time() - start
 
@@ -461,6 +464,19 @@ function skip_offsets(exp,queue)
   end
 end
 
+function prepare!(queue::MomentQueue,t::Float64)
+  if required_delta_t(front(queue)) > 0.0 || very_first(queue)
+    delta_t = required_delta_t(front(queue))
+    prepare!(front(queue),t + delta_t)
+
+    @_ queue begin
+      drop(_,1)
+      takewhile(m -> required_delta_t(m) == 0.0,_)
+      foreach(m -> prepare!(m,t + delta_t),_)
+    end
+  end
+end
+
 function process(exp::Experiment,queue::MomentQueue,event::ExpEvent)
   skip_offsets(exp,queue)
 
@@ -468,9 +484,7 @@ function process(exp::Experiment,queue::MomentQueue,event::ExpEvent)
     moment = front(queue)
     handled = handle(exp,queue,moment,event)
     if handled
-      if !isempty(queue)
-        prepare!(front(queue),time(event))
-      end
+      prepare!(queue,time(event))
       data(exp).next_moment = minimum(map(next_moment_time,data(exp).moments))
     end
   end
@@ -495,9 +509,7 @@ function process(exp::Experiment,queue::MomentQueue,t::Float64)
       data(exp).last_time = run_time
       if handle(exp,queue,moment,run_time)
         d = required_delta_t(moment)
-        if !isempty(queue)
-          prepare!(front(queue),run_time)
-        end
+        prepare!(queue,run_time)
 
         latency = run_time - event_time
 

src/sound.jl
@@ -753,8 +753,9 @@ function play(x::Sound,time::Float64=0.0,channel::Int=0)
       end
     end
   elseif experiment_running()
-    warn("Cannot play a sound immediately. Make sure there is a delay ",
-         "so the sound can be properly prepared.")
+    warn("On trial $(Weber.trial()), offset $(Weber.offset()): Cannot guarntee",
+         " the timing of a sound. Add a delay to the sound if precise timing",
+         " is required.")
   end
 
   # play the sound

src/trial.jl
@@ -435,6 +435,9 @@ end
 This moment starts when either `isresponse` evaluates to true or
 timeout time (in seconds) passes.
 
+The `isresponse` function will be called anytime an event occurs. It should
+take one parameter (the event that just occured).
+
 If the moment times out, the function `fn` (with no arguments) will be called.
 
 If the response is provided before `atleast` seconds, the moment does not begin
@@ -489,14 +492,22 @@ end
 """
     Weber.prepare!(m,[last_moment])
 
-If there is anything the moment needs to do before it occurs, it
-is done during `prepare!`. This triggers immediately after the moment prior to
-the current one has finished. The default implementation does nothing.
+If there is anything the moment needs to do before it occurs, it is done during
+`prepare!`. Prepare can be used to set up precise timing even when hardware
+latency is high, if that latency can be predicted, and accounted for. A moment's
+prepare! method is called just before the first non-zero pause between moments
+that occurs before this moment: in the simplest case, when this moment has a
+non-zero value for [`delta_t`](@ref), preapre! will occur `delta_t` seconds
+before this moment. However, if several moments with no pause occur, prepare!
+will occur before all of those moments as well.
 
-Prepare accepts an optional second argument used to indicate when the previous
-moment began (in seconds from expeirment start). This can permit prepare to set
-up precise timing even when event latency is high, if that latency can be
-predicted, and accounted for.
+Prepare accepts an optional second argument used to indicate the time, in
+seconds from the start of the experiemnt when this moment will begin.
+This argument may be Inf, indicating that it is not possible to predict
+when the moment will occur at this point, because the timing depends on
+some stateful information (e.g. a participant's response). It is accetable
+in this case to throw an error, explaining that this kind of moment
+must be able to know when it occurs sooner.
 
 !!! note
 
@@ -516,11 +527,19 @@ function prepare!(m::DisplayFunctionMoment)
 end
 
 function prepare!(m::PlayMoment,last_moment::Float64)
-  play(m.sound,m.delta_t > 0.0 ? m.delta_t + last_moment : 0.0,m.channel)
+  if !isinf(last_moment)
+    play(m.sound,last_moment,m.channel)
+  else
+    m.prepared = true
+  end
 end
 
 function prepare!(m::PlayFunctionMoment,last_moment::Float64)
-  play(sound(m.fn()),m.delta_t + last_moment,m.channel)
+  if !isinf(last_moment)
+    play(sound(m.fn()),last_moment,m.channel)
+  else
+    m.prepared = Nullable(sound(m.fn()))
+  end
 end
 
 """
@@ -531,8 +550,8 @@ type of moment. The `to_handle` object is either a `Float64`, indicating the
 current time, or it is an `ExpEvent` indicating the event that just occured. A
 timed moment, for instance, will run when it recieves a `Float64` value. The
 queue is a `MomentQueue` object, which has the same interface as the `Dequeue`
-object (from the `DataStructures` package). Upon calling handle, `top(queue) ==
-moment`.
+object (from the `DataStructures` package) but it is also iterable. Upon calling
+handle, `top(queue) == moment`.
 
 Handle returns a boolean indicating whether the event was "handled" or not. If
 unhandled, the moment should remain on top of the queue. If returning true,
@@ -562,11 +581,24 @@ end
 
 run(exp,q,m::TimedMoment) = m.run()
 run(exp,q,m::OffsetStartMoment) = m.run()
-run(exp,q,m::PlayMoment) = nothing
-run(exp,q,m::DisplayMoment) = display(win(exp),m.visual)
-run(exp,q,m::PlayFunctionMoment) = nothing
-run(exp,q,m::DisplayFunctionMoment) = display(win(exp),get(m.visual))
 run(exp,q,m::MomentSequence) = foreach(x -> run(exp,q,x),m.data)
+
+run(exp,q,m::DisplayMoment) = display(win(exp),m.visual)
+run(exp,q,m::DisplayFunctionMoment) = display(win(exp),get(m.visual))
+
+function run(exp,q,m::PlayMoment)
+  if m.prepared
+    m.prepared = false
+    play(m.sound,0.0,m.channel)
+  end
+end
+
+function run(exp,q,m::PlayFunctionMoment)
+  if !isnull(m.prepared)
+    play(get(m.prepared),0.0,m.channel)
+    m.prepared = Nullable()
+  end
+end
 
 function handle(exp::Experiment,q::MomentQueue,
                 moment::AbstractTimedMoment,time::Float64)
@@ -611,11 +643,9 @@ function handle(exp::Experiment,q::MomentQueue,m::ResponseMoment,event::ExpEvent
 end
 
 function handle(exp::Experiment,q::MomentQueue,moments::CompoundMoment,x)
-  queue = Deque{AbstractMoment}()
-  for moment in moments.data
-    push!(queue,moment)
-  end
-  push!(data(exp).moments,MomentQueue(queue,q.last))
+  compq = MomentQueue(moments.data,q.last)
+  push!(data(exp).moments,compq)
+  prepare!(compq,q.last)
   dequeue!(q)
   true
 end
@@ -627,7 +657,7 @@ function handle(exp::Experiment,q::MomentQueue,m::ExpandingMoment,x)
     end
 
     for x in m.data
-      unshift!(q.data,x)
+      unshift!(q,x)
     end
   else
     dequeue!(q)

src/types.jl
@@ -1,5 +1,5 @@
 import Base: show, isempty, time, >>, length, unshift!, promote_rule, convert,
-  hash, ==, isless, pop!, info, next
+  hash, ==, isless, pop!, info, next, start, done, next, getindex
 import DataStructures: front, back, top
 using MacroTools
 
@@ -359,7 +359,6 @@ moment should begin. The default implementation returns zero.
     This method is part of the private interface for moments. It
     should not be called directly, but implemented as part of an extension.
 """
-
 delta_t(m::AbstractMoment) = 0.0
 required_delta_t(m::AbstractMoment) = delta_t(m)
 isimmediate(m::AbstractMoment) = false
@@ -379,6 +378,7 @@ isimmediate(m::ResponseMoment) = false
 
 abstract AbstractTimedMoment <: SimpleMoment
 sequenceable(m::AbstractTimedMoment) = true
+isimmediate(m::AbstractTimedMoment) = delta_t(m) == 0.0
 
 type TimedMoment <: AbstractTimedMoment
   delta_t::Float64
@@ -400,7 +400,9 @@ type PlayMoment <: AbstractTimedMoment
   delta_t::Float64
   sound::Sound
   channel::Int
+  prepared::Bool
 end
+PlayMoment(d,f,c) = PlayMoment(d,f,c,false)
 delta_t(m::PlayMoment) = m.delta_t
 sequenceable(m::PlayMoment) = true
 
@@ -408,7 +410,9 @@ type PlayFunctionMoment <: AbstractTimedMoment
   delta_t::Float64
   fn::Function
   channel::Int
+  prepared::Nullable{Sound}
 end
+PlayFunctionMoment(d,f,c) = PlayFunctionMoment(d,f,c,Nullable())
 delta_t(m::PlayFunctionMoment) = m.delta_t
 sequenceable(m::PlayFunctionMoment) = true
 
@@ -477,18 +481,120 @@ end
 delta_t(m::ExpandingMoment) = 0.0
 isimmediate(m::ExpandingMoment) = false
 
+type EmptyMoment <: AbstractMoment end
+empty_moment = EmptyMoment()
+
 type MomentQueue
-  data::Deque{AbstractMoment}
+  data::Vector{AbstractMoment}
   last::Float64
+  start_index::Int
+  end_index::Int
+  very_first::Bool
 end
-isempty(m::MomentQueue) = isempty(m.data)
-length(m::MomentQueue) = length(m.data)
-enqueue!(m::MomentQueue,x) = push!(m.data,x)
-dequeue!(m::MomentQueue) = shift!(m.data)
-unshift!(m::MomentQueue,x) = unshift!(m.data,x)
-pop!(m::MomentQueue) = pop!(m.data)
-front(m::MomentQueue) = front(m.data)
-back(m::MomentQueue) = back(m.data)
+show(io::IO,q::MomentQueue) = write(io,"MomentQueue[$(join(q,","))]")
+start(m::MomentQueue) = 1
+done(m::MomentQueue,i::Int) = i > length(m)
+next(m::MomentQueue,i::Int) = m[i], i+1
+@inline function getindex(m::MomentQueue,i)
+  @boundscheck 0 < i <= length(m)
+  real_index = i + m.start_index - 1
+  real_index <= length(m.data) ? real_index : real_index - length(m.data)
+  @inbounds return m.data[real_index]
+end
+
+function MomentQueue(xs,last::Float64)
+  size = 2^ceil(Int,log(length(xs))/log(2))
+  data = repeat(AbstractMoment[empty_moment],inner=[size])
+  data[1:length(xs)] = xs
+  MomentQueue(data,last,1,length(xs),true)
+end
+
+MomentQueue(size=256) =
+  MomentQueue(repeat(AbstractMoment[empty_moment],inner=[size]),0.0,1,1,true)
+isempty(m::MomentQueue) = m.data[m.start_index] == empty_moment
+function length(m::MomentQueue)
+  if m.start_index < m.end_index
+    m.end_index - m.start_index + 1
+  elseif m.start_index > m.end_index
+    length(m.data) - m.start_index + m.end_index + 1
+  else
+    Int(m.data[m.start_index] != empty_moment)
+  end
+end
+
+function resize!(mq::MomentQueue,n)
+  new_data = repeat(AbstractMoment[empty_moment],inner=[n])
+  @inbounds for (i,m) in enumerate(mq)
+    new_data[i] = m
+  end
+  mq.data = new_data
+  end_index = length(mq)
+  mq.start_index = 1
+  mq.end_index = end_index
+
+  mq
+end
+
+function enqueue!(mq::MomentQueue,x)
+  if isempty(mq)
+    mq.data[mq.start_index] = x
+  elseif length(mq) < length(mq.data)
+    mq.end_index == length(mq.data) ? mq.end_index = 1 : mq.end_index += 1
+    mq.data[mq.end_index] = x
+  else enqueue!(resize!(mq,2*length(mq.data)),x) end
+  mq
+end
+
+function dequeue!(m::MomentQueue)
+  @assert !isempty(m)
+  result = m.data[m.start_index]
+
+  if length(m) > 1
+    m.data[m.start_index] = empty_moment
+    if m.start_index < length(m.data)
+      m.start_index += 1
+    else
+      m.start_index = 1
+    end
+  else
+    m.data[m.start_index] = empty_moment
+  end
+
+  m.very_first = false
+  result
+end
+
+function unshift!(m::MomentQueue,x)
+  if isempty(m)
+    m.data[m.start_index] = x
+  elseif length(m) < length(m.data)
+    m.start_index > 1 ? m.start_index -= 1 : m.start_index = length(m.data)
+    m.data[m.start_index] = x
+  else unshift!(resize!(m,2*length(m.data)),x) end
+  m
+end
+
+function pop!(m::MomentQueue)
+  @assert !isempty(m)
+  result = m.data[m.end_index]
+
+  if length(m) > 1
+    m.data[m.end_index] = empty_moment
+    if m.end_index == 1
+      m.end_index = length(m.data)
+    else
+      m.end_index -= 1
+    end
+  else
+    m.data[m.end_index] = empty_moment
+  end
+
+  result
+end
+
+front(m::MomentQueue) = m.data[m.start_index]
+back(m::MomentQueue) = m.data[m.end_index]
+very_first(m::MomentQueue) = m.very_first
 function next_moment_time(m::MomentQueue)
   (isempty(m) ? Inf : m.last + delta_t(front(m)))
 end

test/runtests.jl
@@ -1,8 +1,6 @@
 using Weber
 using Base.Test
-
-# NOTE: much of the code is interactive and multimedia driven, none of which can
-# be tested here. I'm just testing some of the timing of experiments here.
+import Weber: prepare!, handle
 
 # this allows a test of timing for an experiment without setting up any
 # multimedia resources, so it can be run just about anywhere.
@@ -10,6 +8,7 @@ function find_timing(fn;keys...)
   empty!(Weber.null_record)
   exp = Experiment(;null_window=true,hide_output=true,keys...)
   setup(() -> fn(),exp)
+
   run(exp,await_input=false)
 
   nostarts = filter(x -> !endswith(string(x[:code]),"_start") &&
@@ -44,9 +43,9 @@ seq_trial_index = map(x -> x[:trial],rows)
 # warm up JIT...
 find_timing() do
   addtrial(moment(0.05,() -> record(:a)),
-           moment(0.1,() -> record(:b)) >>
-             moment(0.1,() -> record(:d)),
-           moment(0.15,() -> record(:c)))
+           moment(0.05,() -> record(:b)) >> moment(0.1,() -> record(:d)),
+           moment(0.1,() -> record(:c)),
+           moment(0.1,() -> record(:e)))
 end
 
 comp_events,comp_times,_ = find_timing() do
@@ -103,6 +102,41 @@ elseif_events,_,_ = find_timing() do
     else
       addtrial(moment(() -> record(:c)))
     end
+  end
+end
+
+type TestPrepareMoment <: Weber.SimpleMoment
+  event::Symbol
+end
+function handle(exp::Weber.Experiment,queue::Weber.MomentQueue,
+                moment::TestPrepareMoment,x)
+  Weber.dequeue!(queue)
+  true
+end
+
+prepare!(moment::TestPrepareMoment) = record(moment.event)
+
+ks,vs,_ = find_timing() do
+  addtrial(TestPrepareMoment(:a),moment(0.5),moment(),moment(),
+           TestPrepareMoment(:b),moment(record,:b_post),moment(0.5))
+  addtrial(moment(),TestPrepareMoment(:c),moment(record,:c_post),
+           timeout(() -> nothing,iskeydown,0.5),TestPrepareMoment(:d))
+  addtrial(moment(0.1),TestPrepareMoment(:e) >> TestPrepareMoment(:f))
+end
+prepare_timing = Dict(k => v for (k,v) in zip(ks,vs))
+
+type TestPrepareError <: Weber.SimpleMoment end
+function handle(exp::Weber.Experiment,queue::Weber.MomentQueue,
+                moment::TestPrepareError,x)
+  Weber.dequeue!(queue)
+  true
+end
+type TestPrepareException <: Exception end
+prepare!(moment::TestPrepareError,time) = isinf(time) ? throw(TestPrepareException()) : nothing
+
+function cause_prepare_error1()
+  find_timing() do
+    addtrial(moment(0.5),timeout(() -> nothing,iskeydown,0.5),TestPrepareError())
   end
 end
 
@@ -187,6 +221,16 @@ const moment_eps = 1e-3
         comp_diff = maximum(diff(comp_times) - 0.05)
         @test comp_diff < moment_eps
       end
+    end
+
+    @testset "Moment Preparation" begin
+      @test :a in keys(prepare_timing)
+      @test :d in keys(prepare_timing)
+      @test :e in keys(prepare_timing)
+      @test :f in keys(prepare_timing)
+      @test abs(prepare_timing[:b_post] - prepare_timing[:b] - 0.5) < 0.25
+      @test abs(prepare_timing[:c_post] - prepare_timing[:c] - 0.5) < 0.25
+      @test_throws TestPrepareException cause_prepare_error1()
     end
 
     @testset "Looping Moments" begin

commit 7f54abb2b48a2328e52c68157c2d2473baef0ec7

Fixed bug that prevented empty moment queues from being removed
Introduced by 41ceae3af134ba806eb026aa72615cf851cc805e

src/experiment.jl
@@ -478,7 +478,7 @@ end
 
 function process(exp::Experiment,queues::Array{MomentQueue},x)
   filter!(queues) do queue
-    !isempty(process(exp,queue,x).data)
+    !isempty(process(exp,queue,x))
   end
 end
 