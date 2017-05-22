14cc8ce4084a585c9f4ad6f3e14f444dd23d1c1e
David Little <david.frank.little@gmail.com>
2017-05-22 10:37:25 -0700

    Added _safe version of extension macros.

--- src/extension_macro.jl
+++ src/extension_macro.jl
@@ -14,6 +14,23 @@ function isdocstr_block(docstr)
   end
 end
 
+function warn_doc(name)
+  """
+
+!!! warning "Do not call inside a package"
+
+    Do not call @$(name) inside of a package or in tests. It should
+    only be used in one-off scripts. If Weber$(name) is not currently installed
+    it will be installed by this macro using `Pkg.add` which can lead to
+    problems when called in packages or tests.
+
+    If you want to include an extension without this behavior you can call
+    @$(name)_safe which mimics @$(name) except that it will never call
+    `Pkg.add`.
+
+"""
+end
+
 """
     @extension [Symbol] begin
       [docstring...]
@@ -32,10 +49,12 @@ macro extension(symbol,docstr)
   if (symbol isa Symbol) & isdocstr_block(docstr)
     mod = Symbol("Weber"string(symbol))
     args = gensym("args")
+    safe_doc = "See documentation for @$(string(symbol))."
     expr = quote
-      export $(Symbol("@"string(symbol)))
+      export $(Symbol("@"string(symbol))), $(Symbol("@"string(symbol)"_safe"))
 
-      @doc $(filter(x -> !isexpr(x,:line),docstr.args)[1]) ->
+      @doc $(filter(x -> !isexpr(x,:line),docstr.args)[1]*
+             warn_doc(string(symbol))) ->
       macro $symbol($args...)
         init_call = :($$(Expr(:quote,mod)).InitExtension())
         append!(init_call.args,Weber.clean_kws($args))
@@ -46,6 +65,16 @@ macro extension(symbol,docstr)
             Pkg.add($$(string(mod)))
             $(Expr(:import,$(Expr(:quote,mod))))
           end
+          $(esc(init_call))
+        end
+      end
+
+      @doc $(safe_doc) ->
+      macro $(Symbol(string(symbol)"_safe"))($args...)
+        init_call = :($$(Expr(:quote,mod)).InitExtension())
+        append!(init_call.args,Weber.clean_kws($args))
+        quote
+          $(Expr(:import,$(Expr(:quote,mod))))
           $(esc(init_call))
         end
       end
